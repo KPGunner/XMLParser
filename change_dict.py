d610e3d5f85a90e0931dee90d225f02fd0e256f5
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-06 10:27:22 -0500

    Add files via upload

---
+++ change_dict.py
@@ -0,0 +1,53 @@
+Changes = [(u""" Warnings in this manual alert personnel to hazards associated
+with the use of hazardous materials. Additional information related
+to hazardous materials is provided in <extref>OPNAVINST 5100.23</extref
+>, Navy Safety and Occupational Health (SOH) Program Manual, <extref
+>NAVSUPINST 5100.27</extref>, Navy Hazardous Material Control Program,
+and the <extref>DoD 6050.05</extref>, DoD Hazard Communication (HAZCOM)
+Program. For each hazardous material used within the Navy, a Material
+Safety Data Sheet (MSDS) must be provided and be available for review
+by users. Consult your local safety and health staff concerning any
+questions regarding hazardous materials, MSDS, personal protective
+equipment requirements, appropriate handling and emergency procedures,
+and disposal guidance.""", u"""Warnings for hazardous materials listed in this manual are designed to warn personnel of
+hazards associated with such items when they come in contact with them by actual use. Additional
+information related to hazardous materials is provided in <extref
+>DoD 6050.05</extref>, Hazard Communication Program, <extref>OPNAVINST
+5100.23</extref>, Safety &amp; Occupational Health (SOH) Program Manual,
+and <extref>OPNAVINST 5100.19</extref>, (SOH) for Forces Afloat, and <extref
+>OPNAVINST 5090.1</extref>, Environmental Readiness Manual. For each
+hazardous material used within the Navy and Marine Corps, a Safety
+Data Sheet (SDS) must be available for review by users in Hazardous
+Materials Information Resource System (HMIRS). Consult your local
+safety and health staff concerning any questions regarding hazardous
+materials, SDSs, personal protective equipment requirements, appropriate
+handling and emergency procedures, and disposal guidance. NAVAIR maintains
+the Aviation Hazardous Materials List (AHML) to assist personnel in
+selecting NSNs for Hazardous Materials (HM) listed in technical publications.
+All HMs required for maintenance must be identified in the AHML. Refer
+to the AHML module in the Ship/Shore/Airborne Aviation Requirements
+(SSAR) website for the current listing. https://ssar.nswc.navy.mil/Default.aspx."""), (u"""The abstract symbol bug 
+shows that a material may contain bacteria or viruses that present a danger to your life or health.""", u"""The abstract 
+symbol shows that a material may contain bacteria or viruses that present a danger to your 
+life or health."""), (u"""The symbol of drops of a liquid onto a hand shows that a material will cause burns or 
+irritation of human skin or tissue.""", """The symbol of a liquid dripping onto a hand shows that the material will 
+cause burns or irritation to human skin or tissue."""), (u"""The symbol of a human hand in a block of ice shows that 
+the material is extremely cold and can injure human skin or tissue.""", """The symbol of a hand in a block of ice 
+shows that the material is extremely cold and can injure human skin or tissue."""), (u"""The symbol of a person 
+wearing goggles shows that the material will injure your eyes.""", u"""The symbol of a person wearing goggles shows 
+that the material will injure the eyes."""), (u"""The symbol of a flame shows that a material can ignite and 
+burn you.""", u"""The symbol of a fire shows that the material may ignite and cause burns."""), (u"""The symbol of a 
+human figure in a cloud shows that vapors of a material present a danger to your life and health through 
+inhalation.""", u"""The symbol of a human figure in a cloud shows that material vapors present a danger to life 
+or health."""), (u"""<extref>ASME Y14.38</extref>, Abbreviations and Acronyms""", u"""<extref>COMNAVAIRFORINST 
+4790.2</extref>"""), (u"""&ldquo;HAZARDOUS MATERIALS WARNINGS&rdquo;""", u"""HAZARDOUS MATERIALS WARNINGS"""), (u"""
+&ldquo;WARNING&rdquo;""", u"""WARNING""")]
+#
+# def changes():
+#     for aPair in Changes:
+#         output = aPair[0]
+#         output2 = aPair[1]
+#         print('\n',output)
+#         print('\n',output2)
+#
+# changes()
---
+++ master_iterator.py
@@ -0,0 +1,23 @@
+from change_dict import *
+import os
+
+# path = input('Copy and paste the folder path and hit enter.')
+path = r'\\cobra\AV-8\Common\Users\Kyle\A1-570BE-MDB-000\XML'
+print('\n', path, '\n')
+
+
+def global_replace():
+    for file in os.listdir(path):
+        if file.endswith('.xml'):
+            with open(path + "\\" + file) as openfile:
+                orig_files = openfile.read()
+                for aPair in Changes:
+                    orig_files = orig_files.replace(aPair[0], aPair[1])
+                    # output = orig_files
+                changed = open(path, 'w')
+                changed.write(orig_files)
+                changed.truncate()
+    print(f'{file} successfully updated')
+
+
+global_replace()
