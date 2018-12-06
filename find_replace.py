b1ffdc569e2313df6d8b60891a524e0acbd26365
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-06 10:30:28 -0500

    Update find_replace.py

--- find_replace.py
+++ find_replace.py
@@ -3,10 +3,8 @@ from change_dict import *
 
 # Get the path to the directory you want to make changes to. #
 # path = input('Copy and paste the folder path and hit enter.')
-path = r'\\cobra\AV-8\Common\Users\Kyle\A1-570BE-MDB-000\XML'
+path = r'YOUR PATH'
 print('\n', "You are working on " +  path, '\n')
-
-# Prompt the user for what text they need to find and replace. #
 
 
 def build_changes():
@@ -34,32 +32,5 @@ def build_changes():
     else:
         exit()
 
-    # replace_data = [(old_data, new_data)]
-    # print(replace_data, '\n')
-    # return replace_data
-
-
+        
 build_changes()
-
-
-# def continue_script():
-#     cont = ('\n', input('Would you like to make these changes? Enter "yes" or "no" ').lower())
-#     if cont == 'yes':
-#         selective_fr()
-#     elif cont == 'no':
-#         print('Goodbye!')
-#         break
-#
-#
-# def selective_fr():
-#     for file in os.listdir(path):
-#         if file.endswith('.xml'):
-#             with open(path + "\\" + file) as openfile:
-#                 orig_files = openfile.read()
-#                 for aPair in replace_data:
-#                     orig_files = orig_files.replace(aPair[0], aPair[1])
-#                     # output = orig_files
-#                 changed = open(path, 'w')
-#                 changed.write(orig_files)
-#                 changed.truncate()
-#     print(f'{file} successfully updated')
