3c4e4852be8047b70603930ccfde1ebf7404c787
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-12 13:59:46 -0500

    Update master_iterator.py

--- master_iterator.py
+++ master_iterator.py
@@ -1,11 +1,15 @@
 from change_dict import *
 import os
+import time
+
+start = time.time()
 
 # path = input('Copy and paste the folder path and hit enter.')
-path = r'Path to your files'
-print('\n', path, '\n')
+path = r'<PATH GOES HERE>'
+new_path = os.mkdir(os.path.join(path, 'Updates'))
+print('\n', f'You are working inside {path}.', '\n')
 
-
+# Iterate through all files in the provided path
 def global_replace():
     for file in os.listdir(path):
         if file.endswith('.xml'):
@@ -13,11 +17,13 @@ def global_replace():
                 orig_files = openfile.read()
                 for aPair in Changes:
                     orig_files = orig_files.replace(aPair[0], aPair[1])
-                    # output = orig_files
-                changed = open(path, 'w')
+                    new_path = os.path.join(path, 'Updates', file)
+                changed = open(new_path, 'w', encoding='utf-8')
                 changed.write(orig_files)
-                changed.truncate()
-    print(f'{file} successfully updated')
+                changed.close()
+                openfile.close()
 
 
 global_replace()
+
+print(f'This took {time.time() - start} seconds to run.')
