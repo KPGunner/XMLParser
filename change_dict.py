ef0de6ebb67750936e8418877c63a77c15657f6b
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-18 09:28:24 -0500

    Delete change_dict.py

--- change_dict.py
+++
@@ -1,3 +0,0 @@
-Changes = [(u"""MULTI LINE string of information that you want to find that will be replaced""", u"""This is the 
-information that will replace the information."""), (u"""Another string of data that you want to replace.""", u"""Information that will
-replace it.""")]
