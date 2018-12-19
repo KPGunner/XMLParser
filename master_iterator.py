7e41e327f151948c9971463e14c871a164c2ae86
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-19 10:57:44 -0500

    Update master_iterator.py

--- master_iterator.py
+++ master_iterator.py
@@ -2,24 +2,22 @@ import csv
 import os
 import time
 import io
-import logging
 
-log = logging.basicConfig(level=logging.DEBUG)
 start = time.time()
 
-# path = input('Copy and paste the folder path and hit enter.')
-path = r'C:\Users\PATH TO FOLDER WITH XML FILES'
+path = input('Copy and paste the folder path and hit enter.')
+# path = r'Z:\Common\Users\Kyle\A1-AV8BB-950-200\XML Files'
 new_path = os.mkdir(os.path.join(path, 'Updates'))
 print('\n', f'You are working inside {path}.', '\n')
 
-# Iterate through all files in the provided path
+
 def global_replace():
     breaker = False
     for file in os.listdir(path):
         if file.endswith('.xml'):
             with io.open(path + "\\" + file, 'r+', newline=None, encoding='utf-8') as openfile:
                 openfile = openfile.read().replace("', '", ' ').replace('\n', ' ')
-                f = open('globals.csv', 'r')
+                f = open('all_globals.csv', 'r')
                 f.seek(0)
                 reader = csv.reader(f)
                 next(reader, None)
@@ -29,10 +27,10 @@ def global_replace():
                         breaker = True
                     openfile = openfile.replace(row[0], row[1])
                     new_path = os.path.join(path, 'Updates', file)
-                    changed = open(new_path, 'w', encoding='utf-8')
                     if row[0] not in openfile and not breaker:
                         print('\n', 'The below changes have been made.')
                         breaker = True
+                changed = open(new_path, 'w', encoding='utf-8')
                 changed.write(openfile)
                 changed.close()
 
