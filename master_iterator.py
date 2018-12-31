0d2519e35bb6460a934c94a47e1b6f1d5db40b13
Cogadh <31373828+KPGunner@users.noreply.github.com>
2018-12-31 13:15:46 -0500

    Update master_iterator.py

--- master_iterator.py
+++ master_iterator.py
@@ -2,13 +2,14 @@ import csv
 import os
 import time
 import io
+import re
 
-start = time.time()
-
-path = input('Copy and paste the folder path and hit enter.')
-# path = r'Z:\Common\UsersPATH TO FOLDER WITH XML FILES'
+# path = input('Copy and paste the folder path and hit enter.')
+path = r'C:\Users\kgunn\PycharmProjects\Global_V1\Test'
 new_path = os.mkdir(os.path.join(path, 'Updates'))
 print('\n', f'You are working inside {path}.', '\n')
+
+start = time.time()
 
 
 def global_replace():
@@ -26,11 +27,12 @@ def global_replace():
                         print('\n', f"Found '{row[0]}' in {file}, replacing with '{row[1]}'.")
                         breaker = True
                     openfile = openfile.replace(row[0], row[1])
+                    openfile = re.sub("<!--(.|\s|\n)*?-->", "", openfile)
                     new_path = os.path.join(path, 'Updates', file)
                     if row[0] not in openfile and not breaker:
-                        print('\n', 'The below changes have been made.')
+                        print('\n', 'The below changes have been made and all comments have been removed.')
                         breaker = True
-                changed = open(new_path, 'w', encoding='utf-8')
+                changed = open(new_path, 'w', encoding="UTF-8")
                 changed.write(openfile)
                 changed.close()
 
